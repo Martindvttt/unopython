14dd286e8a49e543c32edd9074796d9f779c1253
Martindvttt <114313338+Martindvttt@users.noreply.github.com>
2023-01-26 09:46:37 +0100

    update_win_cond

--- uno.py
+++ uno.py
@@ -4,7 +4,7 @@ pioche = [
     "9G", "9G", "8G", "8G", "7G", "7G", "6G", "6G", "5G", "5G", "4G", "4G",
     "3G", "3G", "2G", "2G", "1G", "1G", "0G", "9Y", "9Y", "8Y", "8Y", "7Y",
     "7Y", "6Y", "6Y", "5Y", "5Y", "4Y", "4Y", "3Y", "3Y", "2Y", "2Y", "1Y",
-    "1Y", "0y", "9R", "9R", "8R", "8R", "7R", "7R", "6R", "6R", "5R", "5R",
+    "1Y", "0Y", "9R", "9R", "8R", "8R", "7R", "7R", "6R", "6R", "5R", "5R",
     "4R", "4R", "3R", "3R", "2R", "2R", "1R", "1R", "0R", "9B", "9B", "8B",
     "8B", "7B", "7B", "6B", "6B", "5B", "5B", "4B", "4B", "3B", "3B", "2B",
     "2B", "1B", "1B", "0B"
@@ -58,14 +58,22 @@ def tour(main):
 
 
 def partie():
-    while mainIA or mainJoueur:
-        tour(mainJoueur)
-        tourIA(mainIA)
+    while mainJoueur or mainIA:
+        if len(mainJoueur) >= 1:
+            tour(mainJoueur)
+        elif len(mainJoueur) <= 1:
+            print("Joueur Gagne")
+            break
+        elif len(mainIA) >= 1:
+            tourIA(mainIA)
+        elif len(mainIA) <= 1:
+            print("IA Gagne")
+            break
 
 
 def start():
     random.shuffle(pioche)
-    for i in range(0, 7):
+    for i in range(0, 2):
         mainJoueur.append(pioche.pop())
         mainIA.append(pioche.pop())
     fosse.append(pioche.pop())
