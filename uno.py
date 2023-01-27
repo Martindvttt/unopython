abc31257d1b8ad8da718e5559b87b1df21c6e680
Martindvttt <114313338+Martindvttt@users.noreply.github.com>
2023-01-27 13:04:42 +0100

    +2

--- uno.py
+++ uno.py
@@ -1,4 +1,5 @@
 import random
+import secrets
 
 
 def create_deck():
@@ -15,13 +16,20 @@ def create_deck():
             deck.append(itoa + "G")
             deck.append(itoa + "B")
             deck.append(itoa + "Y")
+    for i in range(0, 3):
+        deck.append("+2R")
+        deck.append("+2G")
+        deck.append("+2B")
+        deck.append("+2Y")
+    for i in range(0, 4):
+        deck.append("+4")
     return deck
 
 
 deck = create_deck()
-fosse = []
-mainJoueur = []
-mainIA = []
+tab_waste = []
+hand_player = []
+hand_ai = []
 
 # ----Display---- #
 
@@ -29,7 +37,7 @@ mainIA = []
 def display(index):
     input_str = {
         1: "La carte piochée à été posée",
-        2: f"Carte posée : {fosse[-1]}",
+        2: f"Carte posée : {tab_waste[-1]}",
         3: "Vous piochez",
         4: "L'IA pioche",
         5: "Joueur gagne",
@@ -39,15 +47,15 @@ def display(index):
     print("|********************** UNO **********************|")
     print()
     print('Michel'.center(50))
-    print(f"{len(mainIA)} cartes restantes".center(50))
+    print(f"{len(hand_ai)} cartes restantes".center(50))
     print()
     print()
     print()
-    print(f"Fosse : {fosse[-1]}".center(50))
+    print(f"Fosse : {tab_waste[-1]}".center(50))
     print()
     print()
     print()
-    print(f"{mainJoueur}".center(50))
+    print(f"{hand_player}".center(50))
     if index < 7:
         print(input_str[index].center(50))
     elif index == 7:
@@ -58,73 +66,109 @@ def display(index):
     return output
 
 
-def piocher(main):
-    main.append(deck.pop())
-    if jouable(main):
-        fosse.append(main.pop())
-        display(1)
-    elif main == mainIA:
+def pick(hand, count):
+    for i in range(0, count):
+        hand.append(deck.pop())
+    if hand == hand_ai:
         display(4)
-    elif main == mainJoueur:
+    elif hand == hand_player:
         display(3)
 
 
-def jouable(main):
-    cartejouable = []
-    for carte in main:
-        chiffreia = carte[0]
-        lettreia = carte[1]
-        cartedefosse = fosse[-1]
-        chiffref = cartedefosse[0]
-        lettref = cartedefosse[1]
-        if chiffreia == chiffref or lettreia == lettref:
-            cartejouable.append(carte)
-    return cartejouable
+def waste(hand, card):
+    if card[0] == "+":
+        if card[1] == "2":
+            tab_waste.append(hand.pop(hand.index(card)))
+            if hand == hand_player:
+                pick(hand_ai, 2)
+            elif hand == hand_ai:
+                pick(hand_player, 2)
+        if card[1] == "4":
+            if hand == hand_player:
+                pick(hand_ai, 4)
+            elif hand == hand_ai:
+                card.append(secrets.choice("R", "V", "B", "Y"))
+                pick(hand_player, 4)
+            tab_waste.append(hand.pop(hand.index(card)))
+    else:
+        tab_waste.append(hand.pop(hand.index(card)))
 
 
-def tourIA(main):
-    cartes = jouable(main)
-    if cartes:
-        random.shuffle(cartes)
-        fosse.append(main.pop(main.index(cartes[-1])))
+def check_card(hand):
+    solutions = []
+    to_compare = tab_waste[-1]
+    for card in hand:
+        if card[0] == "+":
+            if card[1] == "2":
+                number = card[1]
+                letter = card[2]
+            if card[1] == "4":
+                solutions.append(card)
+        else:
+            number = card[0]
+            letter = card[1]
+        if to_compare[0] == "+":
+            to_compare_number = to_compare[1]
+            to_compare_letter = to_compare[2]
+        else:
+            to_compare_number = to_compare[0]
+            to_compare_letter = to_compare[1]
+        if card[0] == "+" or to_compare[0] == "+":
+            if letter == to_compare_letter:
+                solutions.append(card)
+            elif number == to_compare_number:
+                solutions.append(card)
+        else:
+            if number == to_compare_number or letter == to_compare_letter:
+                solutions.append(card)
+    return solutions
+
+
+def tour_ai(hand):
+    cards = check_card(hand)
+    if cards:
+        random.shuffle(cards)
+        waste(hand, cards[-1])
         display(2)
     else:
-        piocher(main)
+        pick(hand, 1)
+        if check_card(hand):
+            tab_waste.append(hand.pop())
+            display(1)
 
 
-def tour(main):
-    if jouable(main):
+def tour_player(hand):
+    if check_card(hand):
         card = display(7)
-        fosse.append(main.pop(main.index(card)))
-        display(2)
+        waste(hand, card)
     else:
-        piocher(main)
+        pick(hand, 1)
 
 
-def partie():
+def launch_game():
     lap = 0
-    while mainJoueur or mainIA:
-        if len(mainJoueur) >= 1 and lap == 0:
-            tour(mainJoueur)
+    while hand_player or hand_ai:
+        if len(hand_player) >= 1 and lap == 0:
+            tour_player(hand_player)
             lap = 1
-        elif len(mainJoueur) < 1:
+        elif len(hand_player) < 1:
             display(5)
             break
-        elif len(mainIA) >= 1 and lap == 1:
-            tourIA(mainIA)
+        elif len(hand_ai) >= 1 and lap == 1:
+            tour_ai(hand_ai)
             lap = 0
-        elif len(mainIA) < 1:
+        elif len(hand_ai) < 1:
             display(6)
             break
 
 
 def start():
     random.shuffle(deck)
-    for i in range(0, 2):
-        mainJoueur.append(deck.pop())
-        mainIA.append(deck.pop())
-    fosse.append(deck.pop())
-    partie()
+    for i in range(0, 7):
+        hand_player.append(deck.pop())
+        hand_ai.append(deck.pop())
+    tab_waste.append(deck.pop())
+    launch_game()
 
 
 start()
